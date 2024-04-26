884ae1cddeccdea8297c8df35b7354b03469bb31
Bo (AIML) Yang <bo_yang6@apple.com>
2024-04-26 09:21:12 +0900

    [SPARK-47842][CONNECT] Use v1 as spark connect go library starting version
    ### What changes were proposed in this pull request?
    
    Use v1 for this Spark Connect Go Client library, instead of previously using v34.
    
    Also regenerate protobuf files based on Spark latest 3.5 release (3.5.1).
    
    ### Why are the changes needed?
    
    There is a recent discussion in Spark community for Spark Operator version naming convention. People like to use version independent of Spark versions. That applies to Spark Connect Go Client as well. Thus change the version here to start from v1.
    
    ### Does this PR introduce _any_ user-facing change?
    
    Yes, Spark Connect Go Client users will use v1 instead of use v34.
    
    ### How was this patch tested?
    
    Run unit test.
    
    Closes #19 from hiboyang/bo-dev-07.
    
    Authored-by: Bo (AIML) Yang <bo_yang6@apple.com>
    Signed-off-by: Hyukjin Kwon <gurwls223@apache.org>

--- client/sql/dataframe.go
+++ client/sql/dataframe.go
@@ -23,7 +23,7 @@ import (
 	"github.com/apache/arrow/go/v12/arrow"
 	"github.com/apache/arrow/go/v12/arrow/array"
 	"github.com/apache/arrow/go/v12/arrow/ipc"
-	proto "github.com/apache/spark-connect-go/v34/internal/generated"
+	proto "github.com/apache/spark-connect-go/v1/internal/generated"
 	"io"
 )
 
