957a4b3476bfe748394a3e0a106311605e216288
Martin Grund <martin.grund@databricks.com>
2024-07-24 15:30:17 +0200

    [SPARK-48982] Properly extract Spark Errors from GRPC Request
    ### What changes were proposed in this pull request?
    Properly extract the Spark Exception information from the GRPC status and the associated error info details. The errors are then associated with a `SparkError` type that allows accessing the metadata.
    
    ### Why are the changes needed?
    Compatibility
    
    ### Does this PR introduce _any_ user-facing change?
    No
    
    ### How was this patch tested?
    Added new UT.
    
    Closes #36 from grundprinzip/SPARK-48982.
    
    Authored-by: Martin Grund <martin.grund@databricks.com>
    Signed-off-by: Martin Grund <martin.grund@databricks.com>

--- spark/client/client.go
+++ spark/client/client.go
@@ -111,8 +111,8 @@ func (s *SparkExecutorImpl) AnalyzePlan(ctx context.Context, plan *proto.Plan) (
 	ctx = metadata.NewOutgoingContext(ctx, s.metadata)
 
 	response, err := s.client.AnalyzePlan(ctx, &request)
-	if err != nil {
-		return nil, sparkerrors.WithType(fmt.Errorf("failed to call AnalyzePlan in session %s: %w", s.sessionId, err), sparkerrors.ExecutionError)
+	if se := sparkerrors.FromRPCError(err); se != nil {
+		return nil, sparkerrors.WithType(se, sparkerrors.ExecutionError)
 	}
 	return response, nil
 }
@@ -126,6 +126,8 @@ func NewSparkExecutor(conn *grpc.ClientConn, md metadata.MD, sessionId string) S
 	}
 }
 
+// NewSparkExecutorFromClient creates a new SparkExecutor from an existing client and is mostly
+// used in testing.
 func NewSparkExecutorFromClient(client proto.SparkConnectServiceClient, md metadata.MD, sessionId string) SparkExecutor {
 	return &SparkExecutorImpl{
 		client:    client,
@@ -156,11 +158,15 @@ func (c *ExecutePlanClient) ToTable() (*types.StructType, arrow.Table, error) {
 
 	for {
 		resp, err := c.responseStream.Recv()
+		// EOF is received when the last message has been processed and the stream
+		// finished normally.
 		if err == io.EOF {
 			break
 		}
-		if err != nil {
-			return nil, nil, sparkerrors.WithType(fmt.Errorf("failed to receive plan execution response: %w", err), sparkerrors.ReadError)
+
+		// If the error was not EOF, there might be another error.
+		if se := sparkerrors.FromRPCError(err); se != nil {
+			return nil, nil, sparkerrors.WithType(se, sparkerrors.ExecutionError)
 		}
 
 		// Process the message
