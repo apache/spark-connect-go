99d6c20db36eaa4f8e960dd12ffbc56bbbdad4f5
Farbod Ahmadian <farbodahmadian2014@gmail.com>
2024-09-17 11:09:03 +0200

    feat: init devenv

---
+++ devenv.nix
@@ -0,0 +1,43 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  # https://devenv.sh/basics/
+  env.GREET = "devenv";
+
+  # https://devenv.sh/packages/
+  packages = with pkgs; [
+    git
+    spark
+  ];
+
+  # https://devenv.sh/languages/
+  languages.java.enable = true;
+  languages.java.jdk.package = pkgs.jdk11; 
+
+  # https://devenv.sh/processes/
+  # processes.cargo-watch.exec = "cargo-watch";
+
+  # https://devenv.sh/services/
+  # services.postgres.enable = true;
+
+  # https://devenv.sh/scripts/
+  scripts.hello.exec = ''
+    echo hello from $GREET
+  '';
+
+  enterShell = ''
+    hello
+    spark-shell --version
+  '';
+
+  # https://devenv.sh/tests/
+  enterTest = ''
+    echo "Running tests"
+    git --version | grep --color=auto "${pkgs.git.version}"
+  '';
+
+  # https://devenv.sh/pre-commit-hooks/
+  # pre-commit.hooks.shellcheck.enable = true;
+
+  # See full reference at https://devenv.sh/reference/options/
+}
